8d1b0642197a661c77949fb20799650620087108
Yuta Toyokawa <kppg42@gmail.com>
2019-03-01 10:50:22 +0900

    add switching nav item logic with sign in

--- src/components/molecules/HeaderNav/index.js
+++ src/components/molecules/HeaderNav/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { NavItem, NavLink } from "../../atoms/Common";
+import { isSignedIn } from "../../../utils/signIn";
 
 export default function HeaderNav() {
   return (
@@ -9,9 +10,20 @@ export default function HeaderNav() {
       <NavItem>
         <NavLink to="/">ホーム</NavLink>
       </NavItem>
-      <NavItem>
-        <NavLink to="/achievements">成績</NavLink>
-      </NavItem>
+      {isSignedIn() ? (
+        <>
+          <NavItem>
+            <NavLink to="/achievements">成績</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/">ログアウト</NavLink>
+          </NavItem>
+        </>
+      ) : (
+        <NavItem>
+          <NavLink to="/">ログイン</NavLink>
+        </NavItem>
+      )}
     </Nav>
   );
 }
---
+++ src/utils/signIn.js
@@ -0,0 +1,3 @@
+export function isSignedIn() {
+  localStorage.getItem("auth_token");
+}
