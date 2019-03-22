201888e0612bdc62d2bd251e584cf7e3791b4c4d
Yuta Toyokawa <kppg42@gmail.com>
2019-03-22 18:57:13 +0900

    adjust ranking design

--- src/components/pages/Ranking.js
+++ src/components/pages/Ranking.js
@@ -1,25 +1,108 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
 import { lifecycle } from "recompose";
 import { connect } from "react-redux";
 
 import BaseLayout from "../templates/BaseLayout";
 import { showRanking } from "../../actions";
+import { ListItem, Title } from "../atoms/Common";
 
 function Ranking(props) {
   const { ranking } = props;
-  const userList = ranking.users.map((user, rank) => {
+  const userList = ranking.users.map((user, index) => {
+    const rank = index + 1;
     return (
-      <li>
-        {rank + 1} {user.name}: {user.score}pt
-      </li>
+      <RankingItem>
+        <RankingCrown>
+          {rank < 4 ? <CrownIcon rank={rank} /> : <ShieldIcon />}
+          <Rank>{rank}</Rank>
+        </RankingCrown>
+        <RankingUserName>{user.name}</RankingUserName>
+        <RankingScore>{user.score}pt</RankingScore>
+      </RankingItem>
     );
   });
   return (
     <BaseLayout>
-      <ul>{userList}</ul>
+      <RankingTitle>ワカッタ!!ランキング</RankingTitle>
+      <RankingContainer>{userList}</RankingContainer>
     </BaseLayout>
   );
 }
+
+const RankingTitle = styled(Title)`
+  text-align: center;
+  margin-bottom: 1rem;
+`;
+
+const RankingContainer = styled.ul`
+  list-style: none;
+  max-width: 500px;
+  padding: 0;
+  border: 3px solid black;
+  margin: 0 auto;
+`;
+
+const RankingItem = styled(ListItem)`
+  display: flex;
+  align-items: center;
+  padding: 0.75rem 1rem;
+  border-bottom: 3px solid black;
+  &:last-child {
+    border: 0;
+  }
+`;
+
+const RankingCrown = styled.div.attrs({
+  className: "fa-stack"
+})`
+  margin-right: 0.5rem;
+`;
+
+const CrownIcon = styled(FontAwesomeIcon).attrs({
+  icon: ["fas", "crown"],
+  className: "fa-stack-1x"
+})`
+  color: ${props => {
+    switch (props.rank) {
+      case 1:
+        return "gold";
+      case 2:
+        return "silver";
+      case 3:
+        return "#CD7F32";
+      default:
+        return "transparent";
+    }
+  }};
+  font-size: 2rem;
+`;
+
+const ShieldIcon = styled(FontAwesomeIcon).attrs({
+  icon: ["fas", "shield-alt"],
+  className: "fa-stack-1x"
+})`
+  color: #ffcc66;
+  font-size: 2rem;
+`;
+
+const Rank = styled.span.attrs({
+  className: "fa-stack-1x"
+})`
+  font-size: 0.9rem;
+  font-weight: bold;
+  margin-top: 0.1rem;
+`;
+
+const RankingUserName = styled.div`
+  font-weight: bold;
+  margin-right: auto;
+`;
+
+const RankingScore = styled.div`
+  font-weight: bold;
+`;
 
 const enhancedRanking = lifecycle({
   componentWillMount() {
--- src/vendor/FontAwesome/index.js
+++ src/vendor/FontAwesome/index.js
@@ -1,3 +1,11 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPencilAlt, faTrashAlt, faQuestionCircle, faLock, faUser } from "@fortawesome/free-solid-svg-icons";
-library.add(faLock, faPencilAlt, faTrashAlt, faUser, faQuestionCircle);
+import {
+  faPencilAlt,
+  faTrashAlt,
+  faQuestionCircle,
+  faLock,
+  faUser,
+  faCrown,
+  faShieldAlt
+} from "@fortawesome/free-solid-svg-icons";
+library.add(faCrown, faLock, faPencilAlt, faShieldAlt, faTrashAlt, faUser, faQuestionCircle);
